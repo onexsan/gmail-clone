e62cbfd2c7be31c38adfa9f431dc8425284f1fc7
Vika Filileeva <v.filileeva@seowork.ru>
2023-01-08 19:19:43 +0400

    gmail-clone

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,140 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import MailTable from './components/MailTable.vue'
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Gmailius</h1>
+    <Suspense>
+      <div>
+        <MailTable />
+      </div>
+      <template #fallback>
+        Loading...
+      </template>
+    </Suspense>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+/* Global Styles */
+button {
+  font-size: 16px;
+  padding: 8px;
+  border-radius: 3px;
+  margin: 5px 10px 5px 0px;
+  cursor: pointer;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+button:disabled {
+  cursor: auto;
+}
+button.selected {
+  cursor: auto;
+  color: black;
+  border-color: black;
+  border-width: 2px;
+}
+.clickable {
+  cursor: pointer;
+}
+input[type='checkbox'] {
+  -webkit-appearance:none;
+  cursor: pointer;
+  width:24px;
+  height:24px;
+  background:white;
+  border-radius: 2px;
+  border: 1px solid #555;
+  position: relative;
+  vertical-align: middle;
+  padding: 10px;
+}
+input[type='checkbox'].partial-check {
+  background: #ABC;
+}
+input[type='checkbox']:checked {
+  background: #679;
+}
+.mb-0 {
+  margin-bottom: 0;
+}
+/* Modal */
+.modal, .overlay {
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+}
+.overlay {
+  opacity: 0.5;
+  background-color: black;
+}
+.modal-card {
+  position: relative;
+  max-width: 80%;
+  margin: auto;
+  margin-top: 30px;
+  padding: 20px;
+  background-color: white;
+  min-height: 500px;
+  z-index: 10;
+  opacity: 1;
+}
+/* Email Modal */
+.email-display {
+  text-align: left;
+}
+/* Mail Table */
+.mail-table {
+  max-width: 1000px;
+  width: 100%;
+  margin: auto;
+  border-collapse: collapse;
+}
+.mail-table tr.read {
+  background-color: #EEE;
+}
+.mail-table tr {
+  height: 40px;
+}
+.mail-table td {
+  border-bottom: 1px solid black;
+  padding: 5px;
+  text-align: left;
+}
+.mail-table tr:first-of-type td {
+  border-top: 1px solid black;
+}
+.mail-table td p {
+  max-height: 1.2em;
+  overflow-y: hidden;
+  margin: 0;
+}
+.mail-table td.date {
+  width: 120px;
+}
+/* Bulk Action Bar */
+.bulk-action-bar {
+  width: 100%;
+  max-width: 1000px;
+  margin: auto;
+  text-align: left;
+  padding-bottom: 8px;
+}
+.bulk-action-bar input {
+  margin: 5px;
+}
+.bulk-action-bar .checkbox {
+  margin-right: 6px;
+  margin-left: 3px;
 }
 </style>
---
+++ src/components/MailTable.vue
@@ -0,0 +1,120 @@
+<template>
+  <button @click="selectScreen('inbox')"
+          :disabled="selectedScreen == 'inbox'">Inbox</button>
+  <button @click="selectScreen('archive')"
+          :disabled="selectedScreen == 'archive'">Archived</button>
+
+  <BulkActionBar :emails="filteredEmails" :selectedScreen="selectedScreen" />
+
+  <table class="mail-table">
+    <tbody>
+      <template v-if="filteredEmails.length">
+        <tr v-for="email in filteredEmails"
+            :key="email.id"
+            :class="['clickable', email.read ? 'read' : '']">
+          <td>
+            <input type="checkbox"
+                   @click="emailSelection.toggle(email)"
+                   :checked="emailSelection.emails.has(email)" />
+          </td>
+          <td @click="openEmail(email)">{{email.from}}</td>
+          <td @click="openEmail(email)">
+            <p><strong>{{email.subject}}</strong> - {{email.body}}</p>
+          </td>
+          <td class="date" @click="openEmail(email)">
+            {{format(new Date(email.sentAt), 'MMM do yyyy')}}
+          </td>
+          <td>
+            <button v-if="!email.archived" @click="handleArchive(email, true)">Archive</button>
+            <button v-else @click="handleArchive(email, false)">Unarchive</button>
+          </td>
+        </tr>
+      </template>
+      <template v-else>
+        <tr>
+          <td colspan="5">
+            Oops! No emails are found :(
+          </td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+
+  <ModalView v-if="openedEmail" @closeModal="openedEmail = null">
+    <MailView :email="openedEmail" @changeEmail="changeEmail" />
+  </ModalView>
+</template>
+
+<script>
+  import { format } from 'date-fns';
+  import axios from 'axios';
+  import MailView from '@/components/MailView.vue';
+  import ModalView from '@/components/ModalView.vue';
+  import BulkActionBar from '@/components/BulkActionBar.vue';
+  import { reactive, ref } from 'vue';
+  import useEmailSelection from '@/composables/use-email-selection'
+  export default {
+    async setup(){
+      let {data: emails} = await axios.get('http://localhost:3000/emails')
+      return {
+        emailSelection: useEmailSelection(),
+        format,
+        emails: ref(emails),
+        openedEmail: ref(null),
+        selectedScreen: ref('inbox')
+      }
+    },
+    components: {
+      MailView,
+      ModalView,
+      BulkActionBar,
+    },
+    computed: {
+      sortedEmails() {
+        return this.emails.sort((e1, e2) => {
+          return e1.sentAt < e2.sentAt ? 1 : -1
+        })
+      },
+      filteredEmails() {
+        if(this.selectedScreen == 'inbox') {
+          return this.sortedEmails.filter(e => !e.archived)
+        } else {
+          return this.sortedEmails.filter(e => e.archived)
+        }
+      }
+    },
+    methods: {
+      selectScreen(newScreen) {
+        this.selectedScreen = newScreen
+        this.emailSelection.clear()
+      },
+      openEmail(email) {
+        this.openedEmail = email
+        if(email) {
+          email.read = true
+          this.updateEmail(email)
+        }
+      },
+      handleArchive(email, value) {
+        email.archived = value
+        this.updateEmail(email)
+      },
+      changeEmail({toggleRead, toggleArchive, save, closeModal, changeIndex}) {
+        let email = this.openedEmail
+        if(toggleRead) { email.read = !email.read }
+        if(toggleArchive) { email.archived = !email.archived }
+        if(save) { this.updateEmail(email) }
+        if(closeModal) { this.openedEmail = null }
+        if(changeIndex) {
+          let emails = this.filteredEmails
+          let currentIndex = emails.indexOf(this.openedEmail)
+          let newEmail = emails[currentIndex + changeIndex]
+          this.openEmail(newEmail)
+        }
+      },
+      updateEmail(email) {
+        axios.put(`http://localhost:3000/emails/${email.id}`, email)
+      }
+    }
+  }
+</script>
